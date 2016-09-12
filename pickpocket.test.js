0e85473b9ec3301cd5680ea3e7fbd35f86e33215
Janis Krasemann <krasemann.janis@gmail.com>
2016-09-12 10:03:10 +0200

    Implement tests vor invalid calls to obtainAccessToken / getAuthorizationURL

--- pickpocket.test.js
+++ pickpocket.test.js
@@ -17,16 +17,36 @@ test('instantiating the module with invalid parameter type boolean', async t =>
     'module creation wihtout parameters should throw an error')
 );
 
-test('output of obtainRequestToken with valid consumer key', t =>
+test('output of obtainRequestToken', async t =>
   p.obtainRequestToken().then((token) => {
     t.is(typeof token, 'string', 'valid call to obtainRequestToken resolves with a string');
   })
 );
 
-test('output of getAuthorizationURL with valid consumer key and request token',
-  t => p.obtainRequestToken().then((token) => {
+test('output of getAuthorizationURL with invalid request token',
+  async t => t.throws(() => p.getAuthorizationURL({ requestToken: true }), Error,
+      'getAuthorizationURL should throw an error when called with an invalid request token')
+);
+
+test('output of getAuthorizationURL without request token',
+  async t => t.throws(() => p.getAuthorizationURL(), Error,
+      'getAuthorizationURL should throw an error when called without a request token')
+);
+
+test('output of getAuthorizationURL with valid request token',
+  async t => p.obtainRequestToken().then((token) => {
     const url = p.getAuthorizationURL({ requestToken: token });
     t.truthy(url.startsWith('http'),
       'valid call to getAuthorizationURL returns a url');
   })
+);
+
+test('output of obtainAccessToken with invalid request token',
+  async t => t.throws(p.obtainAccessToken({ requestToken: true }), Error,
+      'obtainAccessToken should throw an error when called with an invalid request token')
+);
+
+test('output of obtainAccessToken without request token',
+  async t => t.throws(p.obtainAccessToken(), Error,
+      'obtainAccessToken should throw an error when called without a request token')
 );
